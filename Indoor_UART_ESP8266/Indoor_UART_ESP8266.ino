ac4af74ac27017fcd38cc001d371368d58b69f24
CarlVinten <53053617+CarlVinten@users.noreply.github.com>
2025-01-23 13:48:37 +0100

    Update Indoor_UART_ESP8266.ino
    Added doxygen comments

--- Indoor_UART_ESP8266/Indoor_UART_ESP8266.ino
+++ Indoor_UART_ESP8266/Indoor_UART_ESP8266.ino
@@ -1,56 +1,57 @@
-/**
- * @file Indoor_UART_ESP8266.ino
- * @brief Code for receiving weather data from outdoor for monitoring and displaying on the indoor unit.
- *
- * This program reads transmitted weather data (humidity, temperature, wind speed, and rain level) via serial input,
- * sends the data to ThingSpeak, and displays it on an LCD. It also controls a motor based on temperature.
- */
 #include <ESP8266WiFi.h>
 #include <ThingSpeak.h>
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 #include "secrets.h"
 
+/*!
+  @brief The pin that should have been used for driving the fan.
+
+  We wanted to drive a fan when the temperature got to high, to try to cool of the room, where the indoor system is.
+  We had problems with giving enough power to the fan with the esp, without getting noice, so for prrof of concept 
+  an LED lights up, when the temperature is above the threshold.
+*/
 #define MOTOR_PIN D0
+#define TEMPERATURE_THRESHOLD 20
+
 WiFiClient client;
 
+
 //! Every 10 seconds switch data on lcd and read data if possible.
-const int readDataDelay = 10 * 1000;
+const int readDataDelay = 4000;
 
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-
-/**
- * @brief Initializes the weather station components.
- *
- * This function sets up the Serial communication, Wi-Fi connection, motor pin, and LCD display.
- */
+/*!
+  Sets up wifi, the serialport and initializes the LCD.
+*/
 void setup() {
   Serial.begin(115200);
   WiFi.begin(ssid, pass);
 
-  pinMode(MOTOR_PIN, OUTPUT);
-  digitalWrite(MOTOR_PIN, LOW);
+
+
+  pinMode(MOTOR_PIN, OUTPUT);    // Set relay pin as OUTPUT
+  digitalWrite(MOTOR_PIN, LOW);  // Ensure the relay is off initially
 
   lcd.init();
   lcd.clear();
   lcd.backlight();
 }
 
-//! Humidity is initialized to 0. It is between 0% and 100%.
+
 int humidity = 0;
 int temperature = 20;
 int wind_speed = 0;
 int rain_level = 0;
 int sun_up = 0;
 int lcd_controller = 0;
+/*!
+  @brief Loop function for displaying, fetching and uploading data.
 
-/**
- * @brief Main loop for processing weather data.
- *
- * This function reads weather data via Serial, updates ThingSpeak, controls the motor, 
- * and updates the LCD display based on the received data.
- */
+  The loop function fetches data through the UART from a ESP 32,
+  Uploads the data to ThingSpeak and displays it on the LCD.
+*/
 void loop() {
   if (Serial.available()) {
     String data = Serial.readStringUntil('\n');  // Read incoming data
@@ -73,10 +74,14 @@ void loop() {
     Serial.println(temperature);
     Serial.print("Wind Speed: ");
     Serial.println(wind_speed);
+    Serial.print("Sun-up: ");
+    Serial.println(sun_up);
+    Serial.print("rain_level: ");
+    Serial.println(rain_level);
 
     write_ThingSpeak();
     
-    if(temperature > 10){
+    if(temperature > TEMPERATURE_THRESHOLD){
       digitalWrite(MOTOR_PIN, HIGH);
     }else{
       digitalWrite(MOTOR_PIN, LOW);
@@ -85,21 +90,24 @@ void loop() {
   write_lcd();
   delay(readDataDelay);
 
+  
 }
 
-/**
- * @brief Sends weather data to ThingSpeak.
- *
- * This function initializes ThingSpeak communication and updates its fields with 
- * humidity, temperature, wind speed, and rain level.
- */
+
+/*!
+  @brief Function for lcd
+
+  This function has 4 states, that show the data from the weateher station:
+  1: temperature and humidity.
+  2: wind and light outside
+  3: Rain in 0-3 hrs
+  4. Rain in 4-6 hrs
+*/
 void write_ThingSpeak(){
   ThingSpeak.begin(client);
   client.connect(server, 80);
-  //Also read
  
-  Serial.print("rain_level: ");
-  Serial.println(rain_level);
+  
   ThingSpeak.setField(6, rain_level);
   ThingSpeak.setField(2, temperature);
   ThingSpeak.setField(3, humidity);
@@ -109,12 +117,6 @@ void write_ThingSpeak(){
 
 }
 
-/**
- * @brief Updates the LCD display with weather data.
- *
- * This function switches between different screens on the LCD to display 
- * temperature, humidity, wind speed, and rain level predictions.
- */
 void write_lcd(){
   lcd_controller++;
     switch(lcd_controller % 4){
@@ -146,10 +148,19 @@ void write_lcd(){
       lcd.print(wind_speed);
       lcd.setCursor(7, 0);
       lcd.print("m/s");
+      lcd.setCursor(0,1);
+      lcd.print("Sun up:");
+      lcd.setCursor(8, 1);
+      if(sun_up == 1){
+        lcd.print("No");
+      }else{
+        lcd.print("Yes");
+      }
+      
       break;
       case 2:
         lcd.clear();
-        // Temperature on LCD
+        // Rain LCD
         lcd.setCursor(0,0);
         lcd.print("Rain in 0-3 hrs:");
         lcd.setCursor(0, 1);
@@ -161,7 +172,7 @@ void write_lcd(){
       break;
       case 3:
         lcd.clear();
-        // Temperature on LCD
+        // Rain LCD
         lcd.setCursor(0,0);
         lcd.print("Rain in 4-6 hrs:");
         lcd.setCursor(0, 1);
